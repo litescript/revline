006398d699901fb721a5aabc5221e3fb8eb534b4
Peter Brown <brown.peterg@gmail.com>
2025-10-13 19:28:50 -0400

    chore: ignore Vite cache directory

--- frontend/.vite-cache/deps/react_jsx-runtime.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-2KCOZADQ.js";
-import "./chunk-IAWTCREQ.js";
-import "./chunk-4MBMRILA.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
